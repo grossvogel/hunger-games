310ab9a89ce947e6d3342b1ca611fb11b6837d59
Jason <jason@pemulis>
2013-10-06 12:08:36 -0500

    clean up the player class file

--- lib/player.rb
+++ lib/player.rb
@@ -4,20 +4,25 @@
 module HungerGames
 	class Player
 
-		#	Given the round number, your current food, your current reputation, the extra hunt threshold m, and an array of other player reputations,
-		#	create an array (indexed just like other_player_reputations) of 'h' or 's' indicating whether or not you want to hunt with the player at that index
-		#	Note that other_player_reputations is shuffled each time, so you cannot maintain state using the indices to identify other players
+		#	Given the round number, your current food, your current reputation, the extra hunt threshold m, 
+		#	and an array of other player reputations, create an array (indexed just like other_player_reputations) 
+		#	of 'h' or 's' indicating whether or not you want to hunt with the player at that index
+		#	Note that other_player_reputations is shuffled each time, so you cannot maintain state 
+		#	using the indices to identify other players
 		def hunt_choices(round_number, current_food, current_reputation, m, other_player_reputations)
 			raise NotImplementedError
 		end
 
-		#	After hunting each round, you players get an opportunity to examine how things went. the food_earnings array is indexed just like other_player_reputations
-		#	and contains the net result of each hunt, depending on how successfully you and your 'partner' hunted.
+		#	After hunting each round, players get an opportunity to examine how things went. 
+		#	the food_earnings array is indexed just like other_player_reputations 
+		#	in this round's hunt_choices call and contains the net result of each hunt, 
+		#	depending on how successfully you and your 'partner' hunted.
 		def hunt_outcomes(food_earnings)
 			raise NotImplementedError
 		end
 
-		#	At the very end of the round, each player is notified about the overall group performance and the extra hunt bounty.
+		#	At the very end of the round, each player is notified about the 
+		#	overall group performance and the extra hunt bounty.
 		def round_end(award, m, number_hunters)
 			raise NotImplementedError
 		end
@@ -26,6 +31,7 @@ module HungerGames
 			self.class.name.split('::').last
 		end
 
+		#	this is just a silly utility method used by some of the example players.
 		def hunt_with_top_n(number_of_hunts, other_player_reputations)
 			if number_of_hunts >= other_player_reputations.length
 				hunts = other_player_reputations.map { |r| 'h' }
@@ -53,20 +59,6 @@ module HungerGames
 			end
 			
 			hunts
-		end
-	end
-
-	class Dove < Player
-		def hunt_choices(round_number, current_food, current_reputation, m, other_player_reputations)
-			hunts = other_player_reputations.map { |r| 'h' }
-		end
-
-		def hunt_outcomes(food_earnings)
-			#	ignore results
-		end
-
-		def round_end(award, m, number_hunters)
-			#	ignore results
 		end
 	end
 
